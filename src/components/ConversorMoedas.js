90a5d545b4f615b55a2711bdc249c2db13ffe7be
AndersonAslap <anderson.adolfo1998@gmail.com>
2021-03-04 20:06:38 -0300

    Adicionando spinner como forma de layout de loading

--- src/components/ConversorMoedas.js
+++ src/components/ConversorMoedas.js
@@ -1,21 +1,64 @@
-import { Container , Form } from 'react-bootstrap';
+import { Button, Container , Spinner } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 
 import styles from '../styles/components/ConversorMoedas.module.css';
 
 export function ConversorMoedas() {
     return (
         <Container className={styles.conversorMoedasContainer}>
+             
             <header>
                 <h2>Conversor de Moedas</h2>
             </header>
 
             <main>
                 <div>
-                    <Form.Control type="text"/>
+                    <input 
+                        type="text"
+                        placeholder="0"
+                        required
+                        className={styles.conversorMoedasInput}
+                    />
+
+                    <select
+                        as="select" 
+                        className={styles.conversorMoedasInput} 
+                    />
+
+                    <select
+                        as="select" 
+                        className={styles.conversorMoedasInput} 
+                    />
+
+                    <Button 
+                        variant="success" 
+                        className={styles.conversorMoedasInput}
+                    >
+                        Converter &nbsp; <FontAwesomeIcon icon={faArrowCircleRight} />
+                    </Button>
+
                 </div>
                 
                 <div>
-                    2
+                    { 
+                        true ? (
+                            <div>
+                                { 
+                                    true ? (
+                                        <div>
+                                            Coverter
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            Convertido
+                                        </div>
+                                ) }
+                            </div>
+                        ) : (
+                            <Spinner animation="border" variant="success" style={{borderRadius:'100%', width:'50px', height:'50px'}}/>
+                        )  }
                 </div>
             </main>
         </Container>
