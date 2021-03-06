ea4de222cc75092cb40332b026476e8a66e11386
AndersonAslap <anderson.adolfo1998@gmail.com>
2021-03-05 22:08:47 -0300

    Finalizando projeto de conversão de moedas

--- src/components/ConversorMoedas.js
+++ src/components/ConversorMoedas.js
@@ -4,19 +4,26 @@ import { Button, Container , Spinner } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 
+import axios from 'axios';
+
 import styles from '../styles/components/ConversorMoedas.module.css';
 import { useState } from 'react';
 
 export function ConversorMoedas() {
 
+    const FIXER_URL = 'http://data.fixer.io/api/latest?access_key=eba7130a5b2d720ce43eb5fcddd47cc3';
+
     const [isSpinner, setIsSpinner] = useState(false);
     const [isConvertido, setIsConvertido] = useState(false);
     const [isFormValidated, setIsFormValidated] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     const [value, setValue] = useState(1);
 
     const [coinFrom, setCoinFrom] = useState("BRL");
     const [coinTo, setCoinTo] = useState("USD");
+
+    const [result, setResult] = useState('');
 
     console.log()
 
@@ -32,8 +39,44 @@ export function ConversorMoedas() {
         setCoinTo(event.target.value);
     }
 
+    function handleCotacao(data) {
+        if (!data || data.success !== true) {
+            return false;
+        }
+
+        const cotacaoFrom = data.rates[coinFrom];
+        const cotacaoTo = data.rates[coinTo];
+
+        const cotacao = (1 / cotacaoFrom * cotacaoTo) * value;
+        return cotacao.toFixed(2);
+    }
+
+    function handleError() {
+        setResult("Ocorreu um erro no sistema !");
+        setIsConvertido(true);
+        setIsError(true);
+        setTimeout(() => setIsSpinner(false), 500);
+    }
+
     function handleConverter(event){
         event.preventDefault();
+        
+        setIsSpinner(true);
+
+        axios.get(FIXER_URL)
+            .then(response => {
+                const cotacao = handleCotacao(response.data);
+                if (cotacao) {
+                    setResult(`${value} ${coinFrom} = ${cotacao} ${coinTo}`);
+                    setIsConvertido(true);
+                    setIsError(false);
+                    setTimeout(() => setIsSpinner(false), 500);
+                } else {
+                    handleError();
+                }
+                
+            })
+            .catch(error => handleError());
     }
 
     return (
@@ -89,12 +132,12 @@ export function ConversorMoedas() {
                             <div>
                                 { 
                                     isConvertido ? (
-                                        <div>
-                                            convertido
+                                        <div className={!isError ? "bg-success text-white text-center" : "bg-danger text-white text-center"} style={{fontWeight:'bold'}}>
+                                            {result}
                                         </div>
                                     ) : (
-                                        <div>
-                                            converter
+                                        <div className="bg-info text-white text-center" style={{fontWeight:'bold'}}>
+                                            Saiba como está a cotação  atual <br /> da sua moeda
                                         </div>
                                 ) }
                             </div>
